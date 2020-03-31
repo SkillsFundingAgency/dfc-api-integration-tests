b33a350519c4ef8c3fa999d91eca0cc73b7c9719
James Barnes <james@testtrends.co.uk>
2020-03-31 12:58:56 +0100

    Removed hardcoded environment uri

--- DFC.Api.JobProfiles.IntegrationTests/Test/DetailTest.cs
+++ DFC.Api.JobProfiles.IntegrationTests/Test/DetailTest.cs
@@ -35,7 +35,7 @@ namespace DFC.Api.JobProfiles.IntegrationTests.Test
         {
             var apiResponse = await this.authorisedApi.GetByName<JobProfileDetailsAPIResponse>(this.jobProfile.CanonicalName).ConfigureAwait(false);
             Assert.AreEqual(HttpStatusCode.OK, apiResponse.StatusCode, "Job details: Unable to retrieve the job details for a job profile.");
-            new CustomAssert(apiResponse, this.jobProfile).PropertiesMatch();
+            new CustomAssert(apiResponse, this.jobProfile, this.appSettings).PropertiesMatch();
         }
 
         [Test]
